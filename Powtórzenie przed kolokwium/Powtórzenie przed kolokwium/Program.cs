9cc636048e5c69f16a6a32fd948eea456e9787e9
ososinskitomasz <ososinski.tomasz@gmail.com>
2019-12-08 12:20:11 +0100

    08.12.2019

---
+++ Powtórzenie przed kolokwium/Powtórzenie przed kolokwium/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Powtórzenie_przed_kolokwium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Napisz program, który oferuje następujące możliwości:
+             * 1. Rysowanie wieży o podanej przez użytkownika wysokości
+             * 2. Kalkulator
+             * 3. Opcje kolorowania konsoli oraz wieży
+             */
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Witamy w kreatorze wież.");
+            Console.ResetColor();
+
+            uint w, h;
+            string kolor;
+
+            while (true)
+            {
+                Console.WriteLine("Jak szeroka ma być wieża?");
+                
+                try
+                {
+                    w = uint.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (SystemException blad)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(blad);
+                    Console.ResetColor();
+                }
+            }
+            
+            while (true)
+            {
+                Console.WriteLine("Jak wysoka ma być wieża?");
+
+                try
+                {
+                    h = uint.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (SystemException blad)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(blad);
+                    Console.ResetColor();
+                }
+            }
+
+            /*while (true)
+            {
+                Console.WriteLine("Jakiego koloru ma być wieża?");
+                kolor = Console.ReadLine();
+
+                if (kolor == "czerwony" || kolor == "zolty" || kolor == "zielony" || kolor == "niebieski" || kolor == "bialy")
+                {
+                    switch (kolor)
+                    {
+                        case "czerwony":
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            break;
+                        case "zolty":
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            break;
+                        case "zielony":
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            break;
+                        case "niebieski":
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            break;
+                        case "bialy":
+                            Console.ForegroundColor = ConsoleColor.White;
+                            break;
+                    }
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Nie ma takiego koloru!");
+                }
+            }
+            */
+
+            string[] dach = new string[w + 2];//Dach
+            for (int i = 0; i < w + 2; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                dach[i] = "/\\";
+                Console.Write(dach[i]);
+            }
+            Console.ResetColor();
+            Console.WriteLine();
+
+            string[] wieza = new string[h];//Wieza
+            for (int i = 0; i < h; i++)
+            {
+                Console.Write("  ");
+                for (int j = 0; j < w; j++)
+                {
+                    wieza[i] = "[]";
+                    Console.Write(wieza[i]);
+                }
+                Console.WriteLine();
+            }
+
+            string[] ziemia = new string[w + 2];//Ziemia
+            for (int i = 0; i < w + 2; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                ziemia[i] = "##";
+                Console.Write(ziemia[i]);
+            }
+            Console.ResetColor();
+    
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tomasz Ososiński - 08.12.2019 - Kolokwium/Tomasz Ososiński - 08.12.2019 - Kolokwium/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+
+namespace Tomasz_Ososiński___08._12._2019___Kolokwium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("WSB - kolokwium nr 1, Tomasz Ososińśki\n");
+            Console.ResetColor();
+
+            uint a, b, wiek;
+
+            while (true)
+            {
+                Console.WriteLine("Podaj długość boku a prostokąta: ");
+
+                try
+                {
+                    a = uint.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (SystemException blad)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(blad);
+                    Console.ResetColor();
+                }
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Podaj długość boku b prostokąta: ");
+
+                try
+                {
+                    b = uint.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (SystemException blad)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(blad);
+                    Console.ResetColor();
+                }
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Podaj wiek: ");
+
+                try
+                {
+                    wiek = uint.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (SystemException blad)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(blad);
+                    Console.ResetColor();
+                }
+            }
+
+            Console.WriteLine();
+            uint lCD, pole, stopien, x;
+
+            while (true)
+            {
+                while (true)
+                {
+                    Console.WriteLine("Menu główne:");
+                    Console.WriteLine("1. Pole prostokąta");
+                    Console.WriteLine("2. Wyświetl, czy wiek jest liczbą parzystą");
+                    Console.WriteLine("3. Macierz");
+                    Console.WriteLine("4. Funkcja");
+                    Console.WriteLine("0. Wyjdź");
+
+                    try
+                    {
+                        x = uint.Parse(Console.ReadLine());
+                        break;
+                    }
+                    catch (SystemException blad)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(blad);
+                        Console.ResetColor();
+                    }
+                }
+
+                switch (x)
+                {
+                    case 1:
+                        Console.Clear();
+                        pole = a * b;
+                        Console.WriteLine("Bok a: " + a);
+                        Console.WriteLine("Bok b: " + b);
+                        Console.WriteLine("Pole prostokąta: " + pole);
+                        break;
+                    case 2:
+                        if (wiek % 2 == 0)
+                        {
+                            Console.WriteLine("Wiek {0} jest liczbą parzystą.", wiek);
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wiek {0} nie jest liczbą parzystą.", wiek);
+                        }
+                        break;
+                    case 3:
+                        while (true)
+                        {
+                            Console.WriteLine("Podaj stopień macierzy: ");
+
+                            try
+                            {
+                                stopien = uint.Parse(Console.ReadLine());
+                                break;
+                            }
+                            catch (SystemException blad)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine(blad);
+                                Console.ResetColor();
+                            }
+                        }
+
+                        int[] macierz = new int[stopien * stopien];
+
+                        for (int i = 1; i < stopien + 1; i++)
+                        {
+                            for (int j = 1; j < stopien + 1; j++)
+                            {
+                                macierz[j] = j;
+
+                                if (j == stopien)
+                                {
+                                    Console.Write("WSB");
+                                }
+
+                                if (j != stopien)
+                                {
+                                    Console.Write(macierz[j] + (stopien * (i - 1)) + "  ");
+                                }
+                            }
+
+                            Console.WriteLine();
+                        }
+
+                        Console.WriteLine("Suma liczb (nie licząc WSB wynosi " + ((stopien * stopien) - stopien));
+                        break;
+                    case 4:
+                        while (true)
+                        {
+                            Console.WriteLine("Podaj liczbę całkowitą dodatnią: ");
+
+                            try
+                            {
+                                lCD = uint.Parse(Console.ReadLine());
+                                break;
+                            }
+                            catch (SystemException blad)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine(blad);
+                                Console.ResetColor();
+                            }
+                        }
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Twoja szczęśliwa liczba to: " + lCD);
+                        Console.ResetColor();
+
+                        break;
+                    case 0:
+                        break; ;
+
+                }
+
+                if (x == 0)
+                {
+                    break;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
